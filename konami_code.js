7f875fe94517f36f89ce2c0819180537695713c0
Kalpana Swaminathan <kalpana294@gmail.com>
2018-01-15 19:18:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.eventListener{
+  document.eventListener('keydown', function(e)){
   const key = parseInt(e.detail||e.which);
   
   if(key === code[index]){
@@ -13,6 +13,7 @@ function init() {
     }
   }else{
     index = 0;
+  }
   }
 
 }
